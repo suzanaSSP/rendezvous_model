946a0b58d4f4b2d2220f87458855ca38831da5df
SuzanaSSP <suzanasp1026@gmail.com>
2023-06-27 15:49:51 -0600

    push

--- agent.py
+++ agent.py
@@ -1,20 +1,8 @@
 import numpy as np
 import random
-from typing import List
-import math
-
-
-# Simulation parameters
-dt           = 0.1     # time step
-a            = 10      # agent is visible to other agent in time a
-L            = 2000    # Size of box
-N            = 26
-num_agents   = 5
-
-# Deal with NaN bugs
-np.seterr(divide='ignore', invalid='ignore')
 
 class Agent:
+    dt = 0.1
     
     def __init__(self, x, y):
         self.x = x
@@ -26,34 +14,33 @@ class Agent:
 
         self.connections = []
 
-    def find_connections(self, agents):
+    def find_connections(self, agents, num_agents):
         # List of agents that are not self and are not connections already
         foreigners = [other_agent for other_agent in agents if other_agent is not self and other_agent not in self.connections]
+    
+        if len(foreigners) == 0:
+            return None
+        
         # How many agents I want to be connected with, minus those I'm already connection with
-        num_of_connection = (round(random.randint(1, len(foreigners)), 0) ) - len(self.connections)
+        num_of_connection = abs(random.randint(1, min(6, (num_agents//3))) - len(self.connections)) #why absolute value? might cause problems -hannah
         
         self.connections = [random.choice(foreigners) for _ in range(num_of_connection)]
 
         for agent in self.connections:
             if self not in agent.connections:
                 agent.connections.append(self) 
-        
-    
-    def find_distances(self, agents):
-        self.find_connections(agents)
-
+          
+    def find_distances(self):
         for agent in self.connections:
             self.change_x += (agent.x - self.x)
             #self.change_y += (agent.y - self.y)
-    
+
     def move(self):
-        new_x = self.x + (self.change_x * dt)
-        #new_y = self.y + (self.change_y * dt)
-
+        new_x = self.x - (self.change_x * self.dt)
         self.x = new_x
-        #self.y = new_y
+        self.change_x = 0
 
-    def move_with_stubborn_agent(self, stubbron_agent):
+    def move_with_stubborn_agent(self, stubbron_agent): 
         self.change_x = stubbron_agent.x - self.x
 
 
--- main_stubborn_agent.py
+++ main_stubborn_agent.py
@@ -4,36 +4,35 @@ import random
 
 plotRealTime = True
 Nt = 500
-min_pos = 40
-max_pos = 80
-num_agents = 10
-L = 100
+min_pos = 5
+max_pos = 15
+num_agents = 20
+L = 20
 
 
 # Creating agents 
 agents = [Agent(random.randint(min_pos, max_pos), random.randint(min_pos, max_pos)) for i in range(num_agents)]
 
-# Creating connections
-for agent in agents:
-    agent.find_connections(agents, num_agents)
+stubborn_agent = Agent(10, 5)
 
 fig, ax = plt.subplots()
-
 for i in range(Nt):
 
-    x = [agent.x for agent in agents]
-    y = [agent.y for agent in agents]
+    x = [agent.x % L for agent in agents]
+    y = [agent.y % L for agent in agents]
 
     for agent in agents:
-        agent.find_distances() 
-        
+        agent.move_with_stubborn_agent(stubborn_agent)
+
     if plotRealTime or (i == Nt-1):
         plt.cla()
         plt.scatter(x, y, s=10, c='k')
+        ax.scatter(10, 5, s=10, c='y')
         ax.set(xlim=(0, L), ylim=(0, L))
         ax.set_aspect('equal')
-        plt.pause(0.1)
-        
+        plt.pause(0.01)
+
+
     for agent in agents:
         agent.move()
 
