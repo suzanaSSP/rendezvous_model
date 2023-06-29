3fc9fc9e6e85de4d087e71d3f79bd81007472926
suzanaSSP <117389578+suzanaSSP@users.noreply.github.com>
2023-06-29 16:21:07 -0600

    model works with connections

--- agent.py
+++ agent.py
@@ -14,7 +14,7 @@ class Agent:
 
         self.connections = []
 
-    def find_connections(self, agents, num_agents):
+    def create_connections(self, agents, num_agents):
         # List of agents that are not self and are not connections already
         foreigners = [other_agent for other_agent in agents if other_agent is not self and other_agent not in self.connections]
     
@@ -24,11 +24,11 @@ class Agent:
         # How many agents I want to be connected with, minus those I'm already connection with
         num_of_connection = abs(random.randint(1, min(6, (num_agents//3))) - len(self.connections)) #why absolute value? might cause problems -hannah
         
-        self.connections = [random.choice(foreigners) for _ in range(num_of_connection)]
+        self.connections += [random.choice(foreigners) for _ in range(num_of_connection)]
 
         for agent in self.connections:
             if self not in agent.connections:
-                agent.connections.append(self) 
+                agent.connections.append(self)
           
     def find_distances(self):
         for agent in self.connections:
@@ -36,11 +36,10 @@ class Agent:
             #self.change_y += (agent.y - self.y)
 
     def move(self):
-        new_x = self.x - (self.change_x * self.dt)
+        new_x = self.x + (self.change_x * self.dt)
         self.x = new_x
         self.change_x = 0
 
     def move_with_stubborn_agent(self, stubbron_agent): 
         self.change_x = stubbron_agent.x - self.x
-
 
--- main.py
+++ main.py
@@ -9,33 +9,36 @@ max_pos = 80
 num_agents = 10
 L = 100
 
+def main():
+    # Creating agents 
+    agents = [Agent(random.randint(min_pos, max_pos), random.randint(min_pos, max_pos)) for i in range(num_agents)]
 
-# Creating agents 
-agents = [Agent(random.randint(min_pos, max_pos), random.randint(min_pos, max_pos)) for i in range(num_agents)]
-
-# Creating connections
-for agent in agents:
-    agent.find_connections(agents, num_agents)
-
-fig, ax = plt.subplots()
-
-for i in range(Nt):
-
-    x = [agent.x for agent in agents]
-    y = [agent.y for agent in agents]
-
+    # Creating connections
     for agent in agents:
-        agent.find_distances() 
-        
-    if plotRealTime or (i == Nt-1):
-        plt.cla()
-        plt.scatter(x, y, s=10, c='k')
-        ax.set(xlim=(0, L), ylim=(0, L))
-        ax.set_aspect('equal')
-        plt.pause(0.1)
-        
-    for agent in agents:
-        agent.move()
+        agent.create_connections(agents, num_agents)
+
+    fig, ax = plt.subplots()
+
+    for i in range(Nt):
+
+        x = [agent.x for agent in agents]
+        y = [agent.y for agent in agents]
+
+        for agent in agents:
+            agent.find_distances() 
+            
+        if plotRealTime or (i == Nt-1):
+            plt.cla()
+            plt.scatter(x, y, s=10, c='k')
+            ax.set(xlim=(0, L), ylim=(0, L))
+            ax.set_aspect('equal')
+            plt.pause(0.1)
+            
+        for agent in agents:
+            agent.move()
 
 
-plt.show()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
